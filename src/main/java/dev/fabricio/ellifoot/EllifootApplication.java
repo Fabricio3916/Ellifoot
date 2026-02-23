38bbec7206c5155935f1ed653036d364cada2f3f
Fabricio3916 <fabriciopassos3916@gmail.com>
2026-02-23 17:12:38 -0300

    Initial commit

---
+++ src/main/java/dev/fabricio/ellifoot/EllifootApplication.java
@@ -0,0 +1,13 @@
+package dev.fabricio.ellifoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EllifootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EllifootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fabricio/ellifoot/EllifootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fabricio.ellifoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EllifootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
